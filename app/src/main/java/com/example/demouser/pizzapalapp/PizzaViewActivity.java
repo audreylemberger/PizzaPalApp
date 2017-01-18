2bed4bc42a7647578a6903579bb97e80576da9ed
Kat Aiello <aiell22k@mtholyoke.edu>
2017-01-18 18:46:18 -0500

    working on pizzaViewActivity

--- app/src/main/java/com/example/demouser/pizzapalapp/PizzaViewActivity.java
+++ app/src/main/java/com/example/demouser/pizzapalapp/PizzaViewActivity.java
@@ -2,12 +2,23 @@ package com.example.demouser.pizzapalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class PizzaViewActivity extends AppCompatActivity {
+
+    private TextView location;
+    private TextView vendor;
+    private TextView toppings;
+    private CheckBox vegan;
+    private CheckBox vegetarian;
+    private CheckBox
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza_view);
     }
+
+
 }
